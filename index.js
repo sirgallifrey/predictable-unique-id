1ff874a5a02bbf76520f226beaa6523061efeb4e
Adilson Schmitt Junior <adilsonschj@gmail.com>
2016-08-15 11:59:46 -0300

    added getter and setter for nextId

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const internals = {};
 
 internals.nextId = 0;
 
-internals.issueId = function() {
+internals.issueId = function () {
 
     const id = internals.nextId;
     internals.nextId = internals.nextId + 1;
     return id;
+};
+
+internals.setNextId = function (value) {
+    internals.nextId = value;
+};
+
+internals.getNextId = function () {
+    return internals.nextId;
 }
 
 exports.issueId = internals.issueId;
+exports.setNextId = internals.setNextId;
+exports.getNextId = internals.getNextId;
